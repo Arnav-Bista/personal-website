2bbf4b7ca36fa28c88f991e730b302faa0c1de45
Arnav-Bista <arnav.bista12@gmail.com>
2024-08-25 19:13:58 +0545

    404

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="h-[80vh] w-full align-middle justify-center items-center place-content-center text-center">
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+    </div>
+  );
+}
