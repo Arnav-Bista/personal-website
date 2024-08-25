01fa683c7e03df39aa22ab94597323c7c908d9b4
Arnav-Bista <arnav.bista12@gmail.com>
2024-08-25 19:14:12 +0545

    Reactive NarBar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
-
 import assert from 'assert';
 import { usePathname } from 'next/navigation';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import "./navbar.css";
 import Link from 'next/link';
@@ -18,11 +17,43 @@ export default function NavBar(props: NavBarProps) {
 
   assert(
     props.titles.length === props.urls.length,
-    "Titles and URLs must be the same length"
+    "Titles and URLs must be the same length!"
   )
 
-  const [page, setPage] = useState(0);
   const currentPage = usePathname();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    console.log("ADDED");
+
+    const handleClickOutside = (e: MouseEvent) => {
+      console.log(e.target);
+      if (e.target !== document.querySelector('.hamburger-icon')) {
+        setOpen(false);
+      }
+      else {
+        setOpen(!open);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  });
+
+
+  const links = props.titles.map((title, index) => {
+    const className = `link mx-8 text-nowrap ${currentPage === props.urls[index] ? 'selected' : ''}`
+    return <Link
+      key={index}
+      href={props.urls[index]}
+      className={className}
+    >
+      {title.toUpperCase()}
+    </Link>
+  });
 
   return (
     <>
@@ -32,20 +63,14 @@ export default function NavBar(props: NavBarProps) {
             <div className='flex-[4] text-3xl'>
               Arnav Bista
             </div>
-            <div className='flex-[1] flex flex-row justify-evenly items-center w-[60%]'>
-              {
-                ...props.titles.map((title, index) => {
-                  const className = `link mx-8 text-nowrap ${currentPage === props.urls[index] ? 'selected' : ''}`
-                  return <Link
-                    key={index}
-                    href={props.urls[index]}
-                    className={className}
-                  >
-                    {title.toUpperCase()}
-                  </Link>
-                }
-                )
-              }
+            <div className='navbar-links'>
+              {links}
+            </div>
+          </div>
+          <div className={`hamburger ${open ? "open" : ""}`}>
+            <h1 className={`hamburger-icon text-center`}>&#9776;</h1>
+            <div className={`hamburger-dropdown ${open ? '' : 'hidden'}`}>
+              {links}
             </div>
           </div>
         </div>
