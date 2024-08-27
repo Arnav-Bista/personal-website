f3dec974bd33a9d6db7dd79c2f5ddc2bfc27f6ee
Arnav-Bista <arnav.bista12@gmail.com>
2024-08-27 22:14:12 +0545

    WIP v2 on Projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <div>WIP</div>
+  return (
+    <div className="h-[80vh] w-full align-middle justify-center items-center place-content-center text-center">
+      <h1>Working on Showcases</h1>
+      <h5 className="opacity-60">(And finishing unfinished projects 🗿)</h5>
+      <h2>Checkout my <Link className="link" href="https://github.com/Arnav-Bista">Github</Link></h2>
+    </div>
+  );
 }
